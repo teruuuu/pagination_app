88ef2875e764eef817d7f91cdce04048410dfdcd
teru <titlt.aiw6@gmail.com>
2021-10-30 10:11:17 +0900

    初期データの投入

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-  vailidates :body, presence: ture
+  validates :body, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+Faker::Config.locale = :ja
+
+50.times do |i|
+  Post.create!(body: "投稿_#{i}")
+end
